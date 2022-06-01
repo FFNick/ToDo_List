0954bbeafa8d6267a1e86a3aca70574d13da0fab
FF_Nick <dmitrij.petrukh@yandex.ru>
2022-06-01 04:11:20 +0300

    Updates

---
+++ precache-manifest.e739730722a7bcad6415e080d74b851b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "11929946356586c5e447",
+    "url": "/ToDo_List/static/css/main.f423cd10.chunk.css"
+  },
+  {
+    "revision": "11929946356586c5e447",
+    "url": "/ToDo_List/static/js/main.11929946.chunk.js"
+  },
+  {
+    "revision": "42eb0d977ca91bba7312",
+    "url": "/ToDo_List/static/js/1.42eb0d97.chunk.js"
+  },
+  {
+    "revision": "296d1d47c738bb5d9495",
+    "url": "/ToDo_List/static/js/runtime~main.296d1d47.js"
+  },
+  {
+    "revision": "39aaca8ad603b12adf69ddffbfb06235",
+    "url": "/ToDo_List/index.html"
+  }
+];
